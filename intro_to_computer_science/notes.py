0f9ebb426bd9302cb4ea835150531e7d4c60b616
joetechem <joe@techemstudios.com>
2017-10-03 11:14:57 -0400

    notes for day one

---
+++ intro_to_computer_science/notes.py
@@ -0,0 +1,40 @@
+"""
+Class notes for Lucille Brown Mondays - Intro to Computer Science
+"""
+
+# 10/2
+  # General Introductions: name, age, coding experience, techem
+  # What is computer science, computers, programming/coding
+    # terminal:
+      # mkdir student_name
+      # geany helloworld.py
+          print "Hello World!"
+          print "anything you want" -- strings
+          print 22 + 42
+
+          # Variables
+          a = "hello"
+          b = "world"
+          print a + b
+          print a + " " + b --string concatenation
+
+          # DATA Types
+            FLOAT
+            STRING
+            BOOL
+            INT
+            # found in all programming languages
+
+          a = 10
+          b = 5
+          print a + b
+
+          # input
+          a = raw_input("give me a number")
+          b = raw_input("give me another number")
+          print a + b
+
+          # Integers
+          a = int(raw_input("give me a number"))
+          b = int(raw_input("give me another number"))
+          print a + b
